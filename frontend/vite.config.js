b3e7b7af073eb192b9ce8655fdd8a17fa7b8664d
tanvi ghadge <tanvighadge27@gmail.com>
2025-03-22 20:01:01 +0530

    Fix Vercel refresh issue

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -6,4 +6,10 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(),tailwindcss()],
   base:"/",
+  build: {
+    outDir: 'dist',
+  },
+  server: {
+    historyApiFallback: true, // Fix frontend routing issue
+  },
 })
